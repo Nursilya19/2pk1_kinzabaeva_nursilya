2e8db429d55fadfb12502ca74f8abd78099f77ff
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-09-27 14:27:23 +0500

    Добавлена 6 практика

--- PZ_6/Program.cs
+++ PZ_6/Program.cs
@@ -4,19 +4,18 @@
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             int count = 0;
 
-            for (int i = array.Length - 1; i >= 0; i--)
+            for (int i = array.Length - 1; i >= 0; i--) // Проходим по массиву в обратном порядке, начиная с последнего элемента
             {
-                if (array[i] % 2 == 0)
+                if (array[i] % 2 == 0)   // Проверяем, является ли текущий элемент четным
                 {
-                    Console.WriteLine("Индекс: " + i + ", Число: " + array[i]);
-                    count++;
+                    Console.WriteLine("Индекс: " + i + ", Число: " + array[i]);  // Выводим индекс и значение четного числа
+                    count++; // Увеличиваем счетчик четных чисел
                 }
             }
-
-            Console.WriteLine("Количество четных чисел: " + count);
+            Console.WriteLine("Количество четных чисел: " + count);   // Выводим количество найденных четных чисел
         }
     }
 }
