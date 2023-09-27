dceb8ba60999d04f293f18c67039524cbd7521b4
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-09-27 14:22:23 +0500

    Добавлена 1 практика

---
+++ PZ_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение линейной задачи");
+
+            double a = 1; // Вводим данные
+            double b = Math.PI / 4;
+            double c = 1;
+
+            double part1 = Math.Pow(Math.Abs(a * Math.Pow(c, 2) - Math.Pow(b, 3)), 1 / 5) + Math.Log(3 * c); // Разделим выражение на 3 части
+            double part2 = ((Math.Pow(Math.E, 3 * c)) + Math.Pow(c, 2)) / (Math.Sin(2 * c));
+            double part3 = Math.Pow(10, -3) * Math.Sqrt(2157 * a);
+
+            double result = part1 - part2 - part3; // Подсчитываем результат
+
+            Console.WriteLine("Ответ: " + result); // Выводим результат
+        }
+    }
+}
