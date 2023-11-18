d0b80c9adc1fe1a4c0466f1c42305d2ff00b99ad
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-11-18 15:43:50 +0500

    Практика 9 добавлена

---
+++ PZ_9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace PZ_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите символ: ");
+            string inputC = Console.ReadLine();
+            char c = char.Parse(inputC); // Преобразование строки в символ
+
+            Console.Write("Введите строку: ");
+            string text = Console.ReadLine();
+
+            string result = ""; // создаем пустую строку
+            for (int i = 0; i < text.Length; i++)
+            {
+                result += text[i];
+                if (text[i] == c) // если текущий символ равен введенному символу C
+                {
+                    result += c; // удваиваем символ
+                }
+            }
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+}
