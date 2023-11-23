ccd243cc0dbb35b33a45e143b4cb28b230389c9a
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-11-23 20:12:07 +0500

    Добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+            string[] textArray = text.Split('.'); // разделяем текст на предложения с помощью метода Split()
+
+            for (int i = 0; i < textArray.Length - 1; i++) // сортируем предложения по длине
+            {
+                for (int j = i + 1; j < textArray.Length; j++)
+                {
+                    if (textArray[i].Length > textArray[j].Length)
+                    {
+                        string temp = textArray[i];
+                        textArray[i] = textArray[j];
+                        textArray[j] = temp;
+                    }
+                }
+            }
+
+            foreach (string sentence in textArray) // перебираем предложения из текста
+            {
+                int sentenceLength = 0;
+
+                foreach (char c in sentence) // перебираем символы в предложении
+                {
+                    if (!Char.IsSeparator(c))
+                    {
+                        sentenceLength++;
+                    }
+                }
+
+                string simvol = $"{sentence} ({sentenceLength} символов)";
+                Console.WriteLine(simvol);
+            }
+        }
+        }
+    }
