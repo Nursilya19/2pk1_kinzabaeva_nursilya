1a751277baad73cf264675487b506dd95a18bdd4
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-12-01 12:33:25 +0500

    Добавлена 11 практика

---
+++ Methods/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static double GetArea(double radius, double len)
+        {
+            return 3.14 * radius * (len + radius);
+        }
+
+        private static void ArrayGeneration(int n)
+        {
+            int[,] array = new int[n, n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(1, 100);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void P(int a, ref int b)
+        {
+            a = 44; b = 33;
+            Console.WriteLine($" внутри метода {a} {b}");
+        }
+        static void K(int v, out int w)
+        {
+            v = 44; w = 33;
+            Console.WriteLine($" внутри метода {v} {w}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            double r, l;
+            double s;
+            Console.WriteLine("Задача 1. Введите данные для вычисления площади поверхности конуса");
+            Console.Write("Введите значение радиуса основания в см: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение боковой стороны в см: ");
+            l = Convert.ToDouble(Console.ReadLine());
+            s = GetArea(r, l);
+            Console.WriteLine("Площадь поверхности конуса: " + s);
+
+            int size;
+            Console.Write("Задача 2. Введите размер массива: ");
+            size = int.Parse(Console.ReadLine());
+            ArrayGeneration(size);
+
+            int a = 2, b = 4;
+            Console.WriteLine($"Задача 3. до вызова {a} {b}");
+            P(a, ref b);
+            Console.WriteLine($" после вызова {a} {b}");
+
+            int v = 2, w;
+            K(v, out w);
+            Console.WriteLine($"Задача 4. после вызова {v} {w}");
+
+        }
+
+    }
+}
---
+++ PZ_11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты вершин четырехугольника"); //вводим координаты вершин
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            int x3 = int.Parse(Console.ReadLine());
+            int y3 = int.Parse(Console.ReadLine());
+            int x4 = int.Parse(Console.ReadLine());
+            int y4 = int.Parse(Console.ReadLine());
+
+            Distance(x1, y1, x2, y2, out double res1); //значение в параметров используемые в методе для всех сторон
+            Distance(x2, y2, x3, y3, out double res2);
+            Distance(x3, y3, x4, y4, out double res3);
+            Distance(x4, y4, x1, y1, out double res4);
+            double perimetr;
+            perimetr = res1+res2+ res3+res4; 
+            Console.WriteLine("Периметр четырехугольника: {0,5:F2} ", perimetr); //выводим результат
+        }
+
+        static void Distance(int x1, int y1, int x2, int y2, out double r) //создаем метод Distance
+        {
+            r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));  //формула для подсчета одной стороны
+        }
+
+
+    }
+
+} 
