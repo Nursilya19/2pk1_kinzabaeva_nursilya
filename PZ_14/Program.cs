2b6006cab11b100d261357d1254fde3414146450
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-12-06 18:23:32 +0500

    Добавлена 14 практика

---
+++ PZ_14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Text.txt");
+            int sum = 0;
+            int count = 0;
+            Console.WriteLine("Оценки меньше 3 баллов у:");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ');
+                string name = parts[0] + " " + parts[1];
+                int mark = int.Parse(parts[2]);
+
+                if (mark < 3)
+                {
+                    Console.WriteLine(name);
+                }
+                sum += mark;
+                count++;
+            }
+            double average = (double)sum / count;
+            Console.WriteLine($"Средний балл по классу: {{0,5:F1}} ", average);
+        }
+    }
+}
