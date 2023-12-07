7ab0de24d4479c36ba4de579297893aeeaca343f
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-12-07 12:27:27 +0500

    Добавлена 15 практика

--- PZ_14/Program.cs
+++ PZ_14/Program.cs
@@ -1,28 +1,39 @@
 ﻿using System;
 using System.IO;
 namespace PZ_14
+{
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines("file_pz_14.txt"); // Открывает файл и записывает в массив lines
-            int sum = 0; 
-            int count = 0; 
-            Console.WriteLine("Оценки меньше 3 баллов у:"); 
-            foreach (string line in lines)  
+            int sum = 0;
+            int count = 0;
+            Console.WriteLine("Оценки меньше 3 баллов у:");
+            foreach (string line in lines)
             {
                 string[] parts = line.Split(' '); // Разделение строки на части по пробелу
                 string name = parts[0] + " " + parts[1]; // Составление имени из частей строки
-                int mark = int.Parse(parts[2]); // Преобразование третьей части строки в целое число и запись в переменную mark
-                if (mark < 3) 
+                int mark = 0;
+                if (int.TryParse(parts[2], out mark)) // Проверка и преобразование третьей части строки в целое число и запись в переменную mark
                 {
-                    Console.WriteLine(name); 
+                    if (mark < 3)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    sum += mark;
+                    count++;
                 }
-                sum += mark;
-                count++; 
             }
-            double average = (double)sum / count; // Вычисление среднего балла
-            Console.WriteLine($"Средний балл по классу: {{0,5:F1}} ", average); 
+            if (count > 0)
+            {
+                double average = (double)sum / count; // Вычисление среднего балла
+                Console.WriteLine($"Средний балл по классу: {average:F1}"); // Вывод среднего балла с точностью до 1 десятичного знака
+            }
+            else
+            {
+                Console.WriteLine("Файл не содержит оценок.");
+            }
         }
     }
 }
---
+++ PZ_15/Program.cs
@@ -0,0 +1,47 @@
+﻿using System; 
+using System.IO; 
+
+namespace PZ_15 
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Введите полный путь к каталогу:"); 
+            string folderPath = Console.ReadLine(); 
+
+            if (Directory.Exists(folderPath)) // Проверка существования указанного каталога
+            {
+                Console.WriteLine("Содержимое каталогов:"); 
+                DisplayDirectoryContents(folderPath); // Вызов метода для отображения содержимого каталога
+            }
+            else
+            {
+                Console.WriteLine("Каталог не существует"); 
+            }
+        }
+
+        static void DisplayDirectoryContents(string folderPath) 
+        {
+            string[] files = Directory.GetFiles(folderPath); // Получение массива файлов в указанном каталоге
+            string[] subDirectories = Directory.GetDirectories(folderPath); // Получение массива подкаталогов в указанном каталоге
+
+            Console.WriteLine($"Каталог: {folderPath}"); 
+
+            Console.WriteLine("Файлы:"); 
+            foreach (string file in files) 
+            {
+                Console.WriteLine(file);
+            }
+
+            if (subDirectories.Length > 0) // Проверка наличия подкаталогов
+            {
+                Console.WriteLine("\nВложенные каталоги:"); 
+                foreach (string subDir in subDirectories) 
+                {
+                    DisplayDirectoryContents(subDir); // Рекурсивный вызов метода для обхода вложенных каталогов
+                }
+            }
+        }
+    }
+}
