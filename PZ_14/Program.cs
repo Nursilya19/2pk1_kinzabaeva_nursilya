f8e2daabe088a1c01f06c5dcf47758d261f2aaf8
Kinzabaeva Nursilya <143933963+kinzabaeva@users.noreply.github.com>
2023-12-07 09:38:48 +0500

    Добавлена 14 практика

--- PZ_14/Program.cs
+++ PZ_14/Program.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using System.IO;
+
 namespace PZ_14
 {
-    internal class Program
+    internal class Program 
     {
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines("Text.txt");
-            int sum = 0;
-            int count = 0;
-            Console.WriteLine("Оценки меньше 3 баллов у:");
-            foreach (string line in lines)
-            {
-                string[] parts = line.Split(' ');
-                string name = parts[0] + " " + parts[1];
-                int mark = int.Parse(parts[2]);
+            string[] lines = File.ReadAllLines("Text.txt"); // Открывает файл "Text.txt" и записывает в массив lines
+            int sum = 0; 
+            int count = 0; 
+            Console.WriteLine("Оценки меньше 3 баллов у:"); 
 
-                if (mark < 3)
+            foreach (string line in lines) 
+            {
+                string[] parts = line.Split(' '); // Разделение строки на части по пробелу и запись в массив parts
+                string name = parts[0] + " " + parts[1]; // Составление имени из частей строки
+                int mark = int.Parse(parts[2]); // Преобразование третьей части строки в целое число и запись в переменную mark
+
+                if (mark < 3) 
                 {
-                    Console.WriteLine(name);
+                    Console.WriteLine(name); // Вывод имени студента с оценкой меньше 3 на консоль
                 }
-                sum += mark;
-                count++;
+                sum += mark; 
+                count++; 
             }
-            double average = (double)sum / count;
+
+            double average = (double)sum / count; 
             Console.WriteLine($"Средний балл по классу: {{0,5:F1}} ", average);
         }
     }
---
+++ PZ_15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите полный путь к каталогу:");
+            string directory = Console.ReadLine();
+            TraverseDirectory(directory); // Вызываем метод для обхода каталога
+        }
+        static void TraverseDirectory(string directory)
+        {
+            try
+            {
+                string[] subDirectories = Directory.GetDirectories(directory); // Получаем список подкаталогов
+                Console.WriteLine("Содержимое каталога " + directory + ":"); // Выводим содержимое текущего каталога
+                string[] files = Directory.GetFiles(directory);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+                foreach (string subDirectory in subDirectories)
+                {
+                    TraverseDirectory(subDirectory); // Рекурсивно вызываем метод для каждого подкаталога
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при обходе каталога: " + e.Message);
+            }
+        }
+    }
+}
