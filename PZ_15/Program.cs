611d5a9d483166f5aab1e444abf5e89adc800aba
Nursilya Kinzabaeva <146180061+Nursilya19@users.noreply.github.com>
2023-12-07 20:58:24 +0500

    Update Program.cs

--- PZ_15/Program.cs
+++ PZ_15/Program.cs
@@ -1,46 +1,60 @@
-﻿using System; 
-using System.IO; 
+using System;
+using System.IO;
 
-namespace PZ_15 
+namespace PZ_15
 {
-    internal class Program 
+    internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            Console.WriteLine("Введите полный путь к каталогу:"); 
-            string folderPath = Console.ReadLine(); 
-
-            if (Directory.Exists(folderPath)) // Проверка существования указанного каталога
+            Console.WriteLine("Введите полный путь к каталогу:");
+            string folderPath = Console.ReadLine();
+            try
             {
-                Console.WriteLine("Содержимое каталогов:"); 
-                DisplayDirectoryContents(folderPath); // Вызов метода для отображения содержимого каталога
+                if (Directory.Exists(folderPath)) // Проверка существования указанного каталога
+                {
+                    Console.WriteLine("Содержимое каталогов:");
+                    DisplayDirectoryContents(folderPath); // Вызов метода для отображения содержимого каталога
+                }
+                else
+                {
+                    Console.WriteLine("Каталог не существует");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Каталог не существует"); 
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
             }
         }
 
-        static void DisplayDirectoryContents(string folderPath) 
+        // Метод для отображения содержимого каталога
+        static void DisplayDirectoryContents(string folderPath)
         {
-            string[] files = Directory.GetFiles(folderPath); // Получение массива файлов в указанном каталоге
-            string[] subDirectories = Directory.GetDirectories(folderPath); // Получение массива подкаталогов в указанном каталоге
-
-            Console.WriteLine($"Каталог: {folderPath}"); 
-
-            Console.WriteLine("Файлы:"); 
-            foreach (string file in files) 
+            try
             {
-                Console.WriteLine(file);
-            }
+                string[] files = Directory.GetFiles(folderPath); // Получение массива файлов в указанном каталоге
+                string[] subDirectories = Directory.GetDirectories(folderPath); // Получение массива подкаталогов в указанном каталоге
 
-            if (subDirectories.Length > 0) // Проверка наличия подкаталогов
-            {
-                Console.WriteLine("\nВложенные каталоги:"); 
-                foreach (string subDir in subDirectories) 
+                Console.WriteLine($"Каталог: {folderPath}"); // Отображение пути к текущему каталогу
+
+                Console.WriteLine("Файлы:");
+                foreach (string file in files)
                 {
-                    DisplayDirectoryContents(subDir); // Рекурсивный вызов метода для обхода вложенных каталогов
+                    Console.WriteLine(file);
                 }
+
+                if (subDirectories.Length > 0) // Проверка наличия подкаталогов
+                {
+                    Console.WriteLine("\nВложенные каталоги:"); // Исправленный текст для отображения вложенных каталогов
+                    foreach (string subDir in subDirectories)
+                    {
+                        DisplayDirectoryContents(subDir); // Рекурсивный вызов метода для обхода вложенных каталогов
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка при обработке каталога: " + ex.Message);
             }
         }
     }
